bb6c07d5e46283ad2fc5ebf2c03fa3c397aae8c9
Kinzerskiy <kinzzersky@gmail.com>
2023-09-04 13:23:08 +0300

    Initial Commit

---
+++ reusable_buttons_swiftUI/reusable_buttons_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  reusable_buttons_swiftUIApp.swift
+//  reusable_buttons_swiftUI
+//
+//  Created by Mac Pro on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct reusable_buttons_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
